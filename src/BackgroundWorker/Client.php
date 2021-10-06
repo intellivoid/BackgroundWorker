d1121222d15a643420dc2a6d6ab09126456c00e3
Netkas <zixing.narrakas1996@gmail.com>
2021-10-06 09:38:41 -0400

    Added auto-restart even to client worker

--- src/BackgroundWorker/Client.php
+++ src/BackgroundWorker/Client.php
@@ -22,11 +22,75 @@
         private $GearmanClient;
 
         /**
+         * @var bool
+         */
+        private $IgnoreConnectionError = false;
+
+        /**
+         * @var bool
+         */
+        private $AutoRestart = false;
+
+        /**
+         * @var null|int
+         */
+        private $NextRestart = null;
+
+        /**
+         * @var string[]
+         */
+        private $Servers = [];
+
+        /**
          * Client constructor.
          */
         public function __construct()
         {
             $this->GearmanClient = null;
+        }
+
+        /**
+         * Reconnects to the server socket
+         */
+        public function reconnect()
+        {
+            unset($this->GearmanClient);
+            $this->GearmanClient = null;
+
+            $this->GearmanClient = new GearmanClient();
+
+            foreach($this->Servers as $host => $port)
+            {
+                try
+                {
+                    $this->getGearmanClient()->addServer($host, $port);
+                }
+                catch(Exception)
+                {
+                    exit(15);
+                }
+            }
+        }
+
+        /**
+         * Checks if the socket needs to be restarted
+         */
+        public function checkAutoRestart(): void
+        {
+            // Disconnect and reconnect
+            if($this->AutoRestart == true)
+            {
+                if($this->NextRestart == null)
+                {
+                    $this->NextRestart = time() + rand(3600, 7200);
+                }
+
+                if(time() >= $this->NextRestart)
+                {
+                    $this->reconnect();
+                    $this->NextRestart = time() + rand(3600, 7200);
+                }
+            }
         }
 
         /**
@@ -38,6 +102,8 @@
          */
         public function addServer(string $host="127.0.0.1", int $port=4730)
         {
+            $this->Servers[$host] = $port;
+
             try
             {
                 $this->getGearmanClient()->addServer($host, $port);
@@ -53,11 +119,37 @@
          */
         public function getGearmanClient(): GearmanClient
         {
+            $this->checkAutoRestart();
+
             if($this->GearmanClient == null)
             {
                 $this->GearmanClient = new GearmanClient();
             }
 
             return $this->GearmanClient;
+        }
+
+        /**
+         * @return bool
+         */
+        public function isAutoRestart(): bool
+        {
+            return $this->AutoRestart;
+        }
+
+        /**
+         * @param bool $AutoRestart
+         */
+        public function setAutoRestart(bool $AutoRestart): void
+        {
+            $this->AutoRestart = $AutoRestart;
+        }
+
+        /**
+         * @return int|null
+         */
+        public function getNextRestart(): ?int
+        {
+            return $this->NextRestart;
         }
     }
